e8c24ff49f74e513cb8641eb06b8c8954b07e8e8
Daddy <sergoffan@mail.ru>
2022-10-23 01:08:41 +0300

    add HW_01

---
+++ HW_01/Program.cs
@@ -0,0 +1,34 @@
+﻿// Показать двумерный массив размером m×n заполненный вещественными числами
+
+Console.WriteLine("Введи количество строк: ");
+int m = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Введи количество столбцов: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+
+int[,] FillMatrix(int m, int n)
+{
+    int[,] matrix = new int[m, n];
+    Random random = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        matrix[i, j] = random.Next(1, 9);
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine();
+int[,] matrix = FillMatrix(m, n);
+PrintMatrix(matrix);
